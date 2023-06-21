2aa78a9dd228b212a2af659ed90162e6cc5525c5
Kai <134881599+Coded-Silver@users.noreply.github.com>
2023-06-20 20:38:42 -0400

    add functionality for player choosing 'rock'

--- rps.js
+++ rps.js
@@ -1,8 +1,24 @@
 function getComputerChoice() {
     let compChoice = Math.floor(Math.random() * 3)
-    return compChoice === 0 ? 'Rock' :
-           compChoice === 1 ? 'Paper' :
-           'Scissors'
+    return compChoice === 0 ? 'rock' :
+           compChoice === 1 ? 'paper' :
+           'scissors'
     }
 
-    console.log(getComputerChoice())
+let playerChoice = prompt("Rock, paper, or scissors?");
+
+function playRound (playerChoice) {
+    let computerChoice = getComputerChoice()
+    console.log(computerChoice)
+
+    return playerChoice.toLowerCase() === computerChoice ?
+            "It's a tie!" :
+            playerChoice.toLowerCase() === 'rock' && computerChoice === 'paper' ?
+            "You lose!" :
+            playerChoice.toLowerCase() === 'rock' && computerChoice === 'scissors' ?
+            "You win!" :
+            "error"
+    }
+
+
+console.log(playRound(playerChoice))
