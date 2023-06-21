70b67ccee20327d093ebe747712b27a6f99e6e80
Kai <134881599+Coded-Silver@users.noreply.github.com>
2023-06-20 21:30:15 -0400

    change to using for loop and switch

--- rps.js
+++ rps.js
@@ -5,34 +5,65 @@ function getComputerChoice() {
            'scissors'
     }
 
-let playerChoice = prompt("Rock, paper, or scissors?")
-let playerScore = 0
-let computerScore = 0
-let winner = ''
-
-function playRound (playerChoice) {
-    let computerChoice = getComputerChoice()
-    console.log(computerChoice)
-
-    return playerChoice.toLowerCase() === computerChoice ?
-            "It's a tie!" :
-            playerChoice.toLowerCase() === 'rock' && computerChoice === 'paper' ?
-            "You lose!" :
-            playerChoice.toLowerCase() === 'rock' && computerChoice === 'scissors' ?
-            "You win!" :
-            playerChoice.toLowerCase() === 'paper' && computerChoice === 'rock' ?
-            "You win!" :
-            playerChoice.toLowerCase() === 'paper' && computerChoice === 'scissors' ?
-            "You lose!" :
-            playerChoice.toLowerCase() === 'scissors' && computerChoice === 'rock' ?
-            "You lose!" :
-            playerChoice.toLowerCase() === 'scissors' && computerChoice === 'paper' ?
-            "You win!" :
-            "error"
-    }
+// let playerChoice = prompt("Rock, paper, or scissors?")
+// let playerScore = 0
+// let computerScore = 0
+// let winner = ''
 
 function playGame (playerChoice) {
+    // let playerChoice = prompt("Rock, paper, or scissors?")
+    let playerScore = 0
+    let computerScore = 0
+    let winner = ''
+    let computerChoice = getComputerChoice()
 
+    for (let i = 0; i < 5; i++) {
+        let playerChoice = prompt("Rock, paper, or scissors?")
+
+        switch(playerChoice) {
+            case playerChoice === computerChoice:
+                console.log("It's a tie!");
+                break;
+            case playerChoice.toLowerCase() === 'rock' && computerChoice === 'paper':
+                console.log("You lose!");
+                computerScore += 1;
+                break;
+            case playerChoice.toLowerCase() === 'rock' && computerChoice === 'scissors':
+                console.log("You win!");
+                playerScore += 1;
+                break;
+        }
+    }
+    if (playerScore > computerScore) {
+        console.log("You won! Congrats!")
+    } else {
+        console.log("Oh no! You lost!")
+    }
 }
 
-console.log(playRound(playerChoice))
+playGame()
+
+// function playRound (playerChoice) {
+//     let computerChoice = getComputerChoice()
+//     console.log(computerChoice)
+
+//     return playerChoice.toLowerCase() === computerChoice ?
+//             "It's a tie!" :
+//             playerChoice.toLowerCase() === 'rock' && computerChoice === 'paper' ?
+//             "You lose!" :
+//             playerChoice.toLowerCase() === 'rock' && computerChoice === 'scissors' ?
+//             "You win!" :
+//             playerChoice.toLowerCase() === 'paper' && computerChoice === 'rock' ?
+//             "You win!" :
+//             playerChoice.toLowerCase() === 'paper' && computerChoice === 'scissors' ?
+//             "You lose!" :
+//             playerChoice.toLowerCase() === 'scissors' && computerChoice === 'rock' ?
+//             "You lose!" :
+//             playerChoice.toLowerCase() === 'scissors' && computerChoice === 'paper' ?
+//             "You win!" :
+//             "error"
+//     }
+
+
+
+// console.log(playRound(playerChoice))
