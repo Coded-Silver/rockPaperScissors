10fe14c284e7777a12c19ba0afa2bb6706c95f9c
Kai <134881599+Coded-Silver@users.noreply.github.com>
2023-07-10 10:55:29 -0400

    add scorekeeping function and update win/lose/tie

--- rps.js
+++ rps.js
@@ -1,6 +1,7 @@
 const playerText = document.querySelector("#playerText");
 const computerText = document.querySelector("#computerText");
 const resultText = document.querySelector("#resultText");
+const scoreText = document.querySelector("#scoreText");
 const choiceBtns = document.querySelectorAll(".choiceBtn");
 let playerScore = 0;
 let computerScore = 0;
@@ -10,14 +11,18 @@ let score = 0;
 choiceBtns.forEach(button => button.addEventListener('click', () => { 
     player = button.textContent;
     computerTurn();
-    if (checkWinner() == "You win!") {
+    let winner = checkWinner();
+    const win = "You win!";
+    const lose = "You lose!";
+    if (winner.indexOf(win) >= 0) {
         playerScore += 1;
-    } else if (checkWinner() == "You lose!") {
+    } else if (winner.indexOf(lose) >= 0) {
         computerScore += 1;
     }    
     playerText.textContent = `Player: ${playerScore}`;
     computerText.textContent = `Computer: ${computerScore}`;
     resultText.textContent = checkWinner();
+    scoreText.textContent = `Score: ${playerScore} - ${computerScore}`;
 }));
 
 function computerTurn() {
@@ -38,12 +43,12 @@ function computerTurn() {
 
 function checkWinner() {
     if(player == computer) {
-        return "It's a tie!";
+        return "The computer chose " + computer + ". It's a tie!";
     } else if (computer == "ROCK") {
-        return (player == "PAPER") ? "You win!" : "You lose!";
+        return (player == "PAPER") ? "The computer chose ROCK! You win!" : "The computer chose ROCK! You lose!";
     } else if (computer == "PAPER") {
-        return (player == "SCISSORS") ? "You win!" : "You lose!";
+        return (player == "SCISSORS") ? "The computer chose PAPER! You win!" : "The computer chose PAPER! You lose!";
     } else if (computer == "SCISSORS") {
-        return (player == "ROCK") ? "You win!" : "You lose!";
+        return (player == "ROCK") ? "The computer chose SCISSORS! You win!" : "The computer chose SCISSORS! You lose!";
     }
 }
